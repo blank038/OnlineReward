b204db44dc4767d7c0c16be83cf4af292217ffb6
Blank038 <admin@mc9y.com>
2025-02-16 20:57:31 +0800

    🐛 fix: Fixes thread safety problem

--- src/main/java/com/blank038/onlinereward/data/cache/CommonData.java
+++ src/main/java/com/blank038/onlinereward/data/cache/CommonData.java
@@ -1,12 +1,12 @@
 package com.blank038.onlinereward.data.cache;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Blank038
  * @since 1.3.9-SNAPSHOT
  */
 public class CommonData {
-    public static final Map<String, PlayerData> DATA_MAP = new HashMap<>();
+    public static final Map<String, PlayerData> DATA_MAP = new ConcurrentHashMap<>();
 }
